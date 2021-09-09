b7259f8aa81e0d20de1bab2528e185c40cdd2d07
tvannost <tvannost@hotmail.com>
2021-09-09 15:02:57 -0400

    fixed syntax errors

--- main.js
+++ main.js
@@ -7,16 +7,16 @@ function randomValueFromArray(array){
   return array[random];
 }
 var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
-var insertx = ["Willy the Goblin, Big Daddy, Father Christmas"];
-var inserty = ["the soup kitchen, Disneyland, the White House"];
-var insertz = ["spontaneously combusted, melted into a puddle on the sidewalk, turned into a slug and crawled away"];
+var insertx = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+var inserty = ["the soup kitchen", "Disneyland", "the White House"];
+var insertz = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
 randomize.addEventListener('click', result);
 
 function result() {
 var newStory = storyText;
 var xItem = randomValueFromArray(insertx);
-var xItem = randomValueFromArray(insertx)
+var xItem = randomValueFromArray(insertx);
 var yItem = randomValueFromArray(inserty);
 var zItem = randomValueFromArray(insertz);
 
